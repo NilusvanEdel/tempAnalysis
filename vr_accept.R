d975a4a34277596d5899a82b8baf3fa4c0a40371
n-kall <33577035+n-kall@users.noreply.github.com>
2017-11-20 16:34:12 +0000

    vr_analysis plan + practice

--- vr_accept.R
+++ vr_accept.R
@@ -31,6 +31,8 @@
 ## intercept for each subject (see Moen2016)
 ## TODO: check which optimizer to use
 
+
+
 library(lme4)
 
 car_ped.glmm <- glmer(accept == "car" ~ persp +  driver + persp:driver  # fixed effects
@@ -44,3 +46,29 @@ child_adult.glmm <- glmer(accept == "child" ~ persp + driver + persp:driver  # f
 road_sidewalk.glmm <- glmer(accept == "sidewalk" ~ persp + driver + persp:driver  #fixed effects
                             + (1 | subj),  # random effect
                             data = road_sidewalk.data, family = binomial)
+
+
+data <- read.csv("dataframe_data_so_far.csv")
+
+data$Scenario <- 0
+
+
+# figures out which scenario is which
+data <- transform(data, Scenario =
+                            ifelse(Number.of.Adults.2 == 0,"car-sac",
+                                   (ifelse(Number.of.Children > 0, "child",
+                                   (ifelse(Number.of.Adults.2 == 2*Number.of.Adults.1, "sidewalk",
+                                   (ifelse(Number.of.Adults.1 == 0, "sanity",
+                                   (ifelse(Number.of.Adults.2 == 5, "practice",""))))))))))
+
+
+
+data$Participant.ID <- as.factor(data$Participant.ID)
+data$Scenario <- as.factor(data$Scenario)
+data$AV.Car <- as.factor(data$AV.Car)
+data$Perspective <- as.factor(data$Perspective)
+
+carsac.sub <- subset(data, Scenario == "car-sac")
+
+
+conf.lm <- glmer(Selected.Option == "Rechts" ~ Perspective * AV.Car + (1|Participant.ID), data=carsac.sub, family="binomial")
