6779a65718f946d8f8be21577e4b250d8b4dafe7
NilusvanEdel <ledelkott@uos.de>
2017-11-27 15:35:56 +0100

    added the childCSVFileGenerator part in csvFileCreator

--- csvFileCreator.py
+++ csvFileCreator.py
@@ -24,11 +24,64 @@ for file in glob.glob("*.csv"):
             if row[0] == firstRow[0]:
                 continue
             combinedCSV.append(row)
-            
+
 ### writeCombined CSV file
 with open(folder+'combinedCSV.csv', 'w+') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     for i in range(len(combinedCSV)):
         wr.writerow(combinedCSV[i])
 
+### get children Scenarios
+childCSV = []
+childCSV.append(['participant-ID', 'male','av/Car', 'perspective', 'passedSanCheck', 'Trial', 'Decision'])
+counter = 0
+with open(folder +'/'+ 'combinedCSV.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    oldID = 0
+    for row in reader:
+        if counter == 0:
+            counter += 1
+            continue
+        dataSplit = row[0].split()
+        newLine = dataSplit[0:3]
+        if oldID != dataSplit[0]:
+            oldID = dataSplit[0]
+            ### next participant skip practiceTrial
+            continue
+        ### check if Sanity check was passed
+        if dataSplit[5] == '0' and dataSplit[6] == '0' and dataSplit[7] == '1':
+            if (dataSplit[8]=='TRUE' and dataSplit[9]=='Ersteres') or \
+                    (dataSplit[8=='FALSE' and dataSplit[9] == 'Zweiteres']):
+                sanityCheckPassed = True
+            else:
+                sanityCheckPassed = False
+            continue
+        ### if not the childrenScenario skip
+        elif dataSplit[5] == '0':
+            continue
 
+        if dataSplit[3] == '0':
+            newLine.append('Passenger')
+        elif dataSplit[3] == '1':
+            newLine.append('Observer')
+        elif dataSplit[3] == '2':
+            newLine.append('PedSmall')
+        else:
+            newLine.append('PedLarge')
+        newLine.append(sanityCheckPassed)
+        if dataSplit[5] == '1':
+            newLine.append('smallGroups')
+        else:
+            newLine.append('largeGroups')
+        if (dataSplit[8] == 'TRUE' and dataSplit[9] == 'Ersteres') or \
+                (dataSplit[8 == 'FALSE' and dataSplit[9] == 'Zweiteres']):
+            newLine.append('hitChildren')
+        else:
+            newLine.append('hitAdults')
+        childCSV.append(newLine)
+
+### writeChildren CSV file
+with open(folder+'childrenCSV.csv', 'w+') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for i in range(len(childCSV)):
+        wr.writerow(childCSV[i])
