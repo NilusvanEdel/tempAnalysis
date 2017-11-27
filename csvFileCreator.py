86a7e8e8ce181986585369be813923cc4be54574
NilusvanEdel <ledelkott@uos.de>
2017-11-27 13:51:46 +0100

    file to create the combinedCSV file out of the single data files

--- csvFileCreator.py
+++ csvFileCreator.py
@@ -1,14 +1,34 @@
 import csv
+import glob
+import os
 
-path = '/home/nilus/Desktop/tempAnalysis/dataframe_data_so_far(1).csv'
-folder = '/home/nilus/Desktop/tempAnalysis/'
+### where the dataFiles are stored
+path = '/home/nilus/PycharmProjects/tempAnalysis/Data'
+### where the created Files will be stored
+folder = '/home/nilus/PycharmProjects/tempAnalysis/'
 
-data = []
-with open(path) as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for column in reader:
-        data.append(column[6:11])
-with open(folder+'new.csv', 'w') as myfile:
+os.chdir(path)
+fileList = []
+combinedCSV = []
+counter = 0
+### get each file in path with ending csv
+for file in glob.glob("*.csv"):
+    with open(path +'/'+ file) as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+            if counter == 0:
+                counter += 1
+                firstRow = row
+                combinedCSV.append(firstRow)
+                continue
+            if row[0] == firstRow[0]:
+                continue
+            combinedCSV.append(row)
+            
+### writeCombined CSV file
+with open(folder+'combinedCSV.csv', 'w+') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    for i in range(len(data)):
-        wr.writerow(data[i])
+    for i in range(len(combinedCSV)):
+        wr.writerow(combinedCSV[i])
+
+
