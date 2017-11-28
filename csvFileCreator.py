b458d6ee81c88f931ea68a881fc4bc079fa5a4d1
NilusvanEdel <ledelkott@uos.de>
2017-11-28 13:59:18 +0100

    fixed a bug, added the creation of selfSac.csv and sidewalk.csv

--- csvFileCreator.py
+++ csvFileCreator.py
@@ -31,9 +31,13 @@ with open(folder+'combinedCSV.csv', 'w+') as myfile:
     for i in range(len(combinedCSV)):
         wr.writerow(combinedCSV[i])
 
-### get children Scenarios
-childCSV = []
-childCSV.append(['participant-ID', 'male','av/Car', 'perspective', 'passedSanCheck', 'Trial', 'Decision'])
+### write first line
+childCSV, sidewalkCSV, selfSacCSV = [], [], []
+firstLine = ['participant-ID', 'male','av/Car', 'perspective', 'passedSanCheck', 'Trial', 'Decision']
+childCSV.append(firstLine)
+sidewalkCSV.append(firstLine)
+selfSacCSV.append(firstLine)
+
 counter = 0
 with open(folder +'/'+ 'combinedCSV.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ')
@@ -48,18 +52,14 @@ with open(folder +'/'+ 'combinedCSV.csv') as csvfile:
             oldID = dataSplit[0]
             ### next participant skip practiceTrial
             continue
-        ### check if Sanity check was passed
+        ### check if sanity check was passed
         if dataSplit[5] == '0' and dataSplit[6] == '0' and dataSplit[7] == '1':
-            if (dataSplit[8]=='TRUE' and dataSplit[9]=='Ersteres') or \
-                    (dataSplit[8=='FALSE' and dataSplit[9] == 'Zweiteres']):
+            if (dataSplit[8] == 'True' and dataSplit[9]=='Ersteres') or \
+                    (dataSplit[8] == 'False' and dataSplit[9] == 'Zweiteres'):
                 sanityCheckPassed = True
             else:
                 sanityCheckPassed = False
             continue
-        ### if not the childrenScenario skip
-        elif dataSplit[5] == '0':
-            continue
-
         if dataSplit[3] == '0':
             newLine.append('Passenger')
         elif dataSplit[3] == '1':
@@ -69,19 +69,59 @@ with open(folder +'/'+ 'combinedCSV.csv') as csvfile:
         else:
             newLine.append('PedLarge')
         newLine.append(sanityCheckPassed)
-        if dataSplit[5] == '1':
-            newLine.append('smallGroups')
-        else:
-            newLine.append('largeGroups')
-        if (dataSplit[8] == 'True' and dataSplit[9] == 'Ersteres') or \
-                (dataSplit[8] == 'False' and dataSplit[9] == 'Zweiteres'):
-            newLine.append('hitChildren')
-        else:
-            newLine.append('hitAdults')
-        childCSV.append(newLine)
+        newLineChild = newLine
+        newLineSidewalk = newLine
+        newLineSelfSac = newLine
+        ### if childrenScenario
+        if dataSplit[5] != '0':
+            if dataSplit[5] == '1':
+                newLineChild.append('smallGroups')
+            else:
+                newLineChild.append('largeGroups')
+            if (dataSplit[8] == 'True' and dataSplit[9] == 'Ersteres') or \
+                    (dataSplit[8] == 'False' and dataSplit[9] == 'Zweiteres'):
+                newLineChild.append('hitChildren')
+            else:
+                newLineChild.append('hitAdults')
+            childCSV.append(newLineChild)
+        ### if sidewalkScenario
+        if (dataSplit[6] == '1' and dataSplit[7] == '2') or \
+                (dataSplit[6] == '2' and dataSplit[7] == '4'):
+            if dataSplit[6]== '1':
+                newLineSidewalk.append('smallGroups')
+            else:
+                newLineSidewalk.append('largeGroups')
+            if (dataSplit[8] == 'True' and dataSplit[9] == 'Ersteres') or \
+                    (dataSplit[8] == 'False' and dataSplit[9] == 'Zweiteres'):
+                newLineSidewalk.append('hitSidewalk')
+            else:
+                newLineChild.append('hitStreet')
+            sidewalkCSV.append(newLineChild)
+        ### if selfSacrificeScenario
+        if dataSplit[6] == '2' and dataSplit[7] == '0':
+            if dataSplit[4] == '2':
+                newLineSelfSac.append('mountain')
+            else:
+                newLineSelfSac.append('cityR')
+            if (dataSplit[8] == 'True' and dataSplit[9] == 'Ersteres') or \
+                    (dataSplit[8] == 'False' and dataSplit[9] == 'Zweiteres'):
+                newLineSelfSac.append('hitPedestrians')
+            else:
+                newLineSelfSac.append('selfSacrifice')
+            selfSacCSV.append(newLineSelfSac)
 
 ### writeChildren CSV file
 with open(folder+'childrenCSV.csv', 'w+') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     for i in range(len(childCSV)):
         wr.writerow(childCSV[i])
+### write sidewalkCSV
+with open(folder+'sidewalkCSV.csv', 'w+') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for i in range(len(sidewalkCSV)):
+        wr.writerow(sidewalkCSV[i])
+### write selfSacCSV
+with open(folder+'selfSacCSV.csv', 'w+') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for i in range(len(selfSacCSV)):
+        wr.writerow(selfSacCSV[i])
