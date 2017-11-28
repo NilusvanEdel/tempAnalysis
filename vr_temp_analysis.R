aa132c4d684b51424e19fc4792a255972aea1b1e
alien <alien@alienware>
2017-11-28 16:39:12 +0100

    clean up variable names and levels

--- vr_temp_analysis.R
+++ vr_temp_analysis.R
@@ -7,6 +7,35 @@ carsac.data <- read.csv("data/selfSacCSV.csv")
 
 sidewalk.data <- read.csv("data/sidewalkCSV.csv")
 
+# clean the factor labels
+
+colnames(child.data)[2] <- "gender"
+colnames(carsac.data)[2] <- "gender"
+colnames(sidewalk.data)[2] <- "gender"
+
+colnames(child.data)[3] <- "driver"
+colnames(carsac.data)[3] <- "driver"
+colnames(sidewalk.data)[3] <- "driver"
+
+child.data$driver <- factor(child.data$driver, levels = c("False","True"),
+                            labels = c("Human", "AV"))
+
+child.data$gender <- factor(child.data$gender, levels = c("False","True"),
+                            labels = c("Female", "Male"))
+
+carsac.data$driver <- factor(carsac.data$driver, levels = c("False","True"),
+                            labels = c("Human", "AV"))
+
+carsac.data$gender <- factor(carsac.data$gender, levels = c("False","True"),
+                            labels = c("Female", "Male"))
+
+
+sidewalk.data$driver <- factor(sidewalk.data$driver, levels = c("False","True"),
+                            labels = c("Human", "AV"))
+
+sidewalk.data$gender <- factor(sidewalk.data$gender, levels = c("False","True"),
+                            labels = c("Female", "Male"))
+
 # only include those that passed sanity
 child.data <- subset(child.data, passedSanCheck == "True")
 carsac.data <- subset(carsac.data, passedSanCheck == "True")
@@ -18,22 +47,22 @@ child.glmm <- glmer(Decision == "hitChildren" ~ perspective * av.Car +
                         (1|participant.ID) + male, family = "binomial", data=child.data)
 
 carsac.glmm <- glmer(Decision == "selfSacrifice" ~ perspective *
-                         av.Car + (1|participant.ID) + male, family = "binomial", data=carsac.data)
+                         driver + (1|participant.ID) + male, family = "binomial", data=carsac.data)
 
-sidewalk.glmm <- glmer(Decision == "hitSidewalk" ~ perspective * av.Car +
+sidewalk.glmm <- glmer(Decision == "hitSidewalk" ~ perspective * driver +
                            (1|participant.ID) + male, family = "binomial",
                        data=sidewalk.data)
 
 # plot the proportions
-child.plot <- ggplot(child.data,aes(x = av.Car,fill = Decision)) +
+child.plot <- ggplot(child.data,aes(x = driver,fill = Decision)) +
     geom_bar(position = "fill") + facet_grid (.~perspective) +
     xlab("Driver, Perspective") + ylab("Proportion of responses")
 
-carsac.plot <- ggplot(carsac.data,aes(x = av.Car,fill = Decision)) +
+carsac.plot <- ggplot(carsac.data,aes(x = driver,fill = Decision)) +
     geom_bar(position = "fill") + facet_grid (.~perspective) +
     xlab("Driver, Perspective") + ylab("Proportion of responses")
 
 sidewalk.plot <-
-    ggplot(sidewalk.data,aes(x = av.Car,fill = Decision)) +
+    ggplot(sidewalk.data,aes(x = driver,fill = Decision)) +
     geom_bar(position = "fill") + facet_grid (.~perspective) +
     xlab("Driver, Perspective") + ylab("Proportion of responses")
