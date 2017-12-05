a8dae8388005d9d62a0ae576cdc5f6869ceea7c8
NilusvanEdel <ledelkott@uos.de>
2017-12-05 16:37:46 +0100

    added the script to create a readable csv file for the online study

---
+++ csvFileCreatorOnlinestudy.py
@@ -0,0 +1,106 @@
+import csv
+import glob
+import os
+
+path = "/home/nilus/PycharmProjects/tempAnalysis/"
+csvFile =  "/home/nilus/PycharmProjects/tempAnalysis/online_accept_data_nov30.csv"
+fullCSV = []
+counter = 0
+with open(csvFile) as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    for row in reader:
+        if counter == 0:
+            firstRow = row
+            counter += 1
+        else:
+            fullCSV.append(row)
+# remove empty rows
+for row in fullCSV:
+    if not any(row):
+        fullCSV.remove(row)
+        print("row removed")
+# remove empty columns
+# transpose the matrix so index of firstRow equals row of matrix
+fullCSV = list(map(list, zip(*fullCSV)))
+for column in fullCSV:
+    if not any(column):
+        del (firstRow[fullCSV.index(column)])
+        fullCSV.remove(column)
+        print("column removed")
+
+# remove 'completed'
+fullCSV.pop(0)
+del (firstRow[0])
+# remove 'session nr, Session_Name, Session_Start_Time, Session_End_Time, Block_Nr, Block_Name, Task_Nr
+for i in range(7):
+    fullCSV.pop(2)
+    del (firstRow[2])
+# remove something
+for i in range(2):
+    fullCSV.pop(3)
+    del firstRow[3]
+
+# add several rows into one (first_vid etc.)
+import collections
+duplicates =  [item for item, count in collections.Counter(firstRow).items() if count > 1]
+print(duplicates)
+for item in duplicates:
+    index = [i for i, x in enumerate(firstRow) if x == item]
+    print(index)
+    counter = 0
+    for i in index:
+        if i == index[0]: continue
+        fullCSV[index[0]] = [a + b for a, b in zip(fullCSV[index[0]], fullCSV[i-counter])]
+        del (firstRow[i - counter])
+        fullCSV.pop(i - counter)
+        counter += 1
+    print(fullCSV[index[0]])
+
+#remove Agent_Spec and Crowdsourcing_Code
+for i in range(2):
+    del (firstRow[-1])
+    fullCSV.pop(-1)
+# remove own SUBJECT id
+del (firstRow[6])
+fullCSV.pop(6)
+
+# transpose the matrix again
+fullCSV = list(map(list, zip(*fullCSV)))
+
+
+#_________________________________________________________________________________________________
+#_________________________________________________________________________________________________
+# remove IDs who failed sanity Check
+oldID = -1
+failedSanCheck = []
+for row in fullCSV:
+  if row[0] != oldID:
+      sanCheckHappened = False
+      passedSanCheck = False
+      oldID = row[0]
+      ind = fullCSV.index(row)
+      # remove the first two rows (disclaimer and instructions)
+      for i in range(2): fullCSV.pop(ind)
+      continue
+  if sanCheckHappened and passedSanCheck: continue
+  else:
+      if not sanCheckHappened:
+          if row[6] == 'swerve':
+              sanCheckHappened = True
+              passedSanCheck = True
+              fullCSV.remove(row)
+          else:
+              failedSanCheck.append(oldID)
+              sanCheckHappened = True
+              fullCSV.remove(row)
+      # if participant failed the sanChecked
+      else:
+          fullCSV.remove(row)
+#_______________________________________________________________________________________________
+#_______________________________________________________________________________________________
+
+with open(path+'shortenedOnlineCSV.csv', 'w+') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(firstRow)
+    for i in range(len(fullCSV)):
+        wr.writerow(fullCSV[i])
