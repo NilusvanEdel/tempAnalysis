a615ecd3e365ae545d7a7b00925e96fa447fa0d7
n-kall <noa@tutamail.com>
2018-01-24 19:07:37 +0100

    add script to install dependencies

---
+++ install_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript -e "install.packages(c('knitr', 'lme4', 'afex, 'cowplot', 'DHARMa', 'xtable', 'rockchalk', 'tikzDevice', 'parallel', 'fifer', 'multcomp', 'vcdExtra'), repos='https://cran.uni-muenster.de/')"
