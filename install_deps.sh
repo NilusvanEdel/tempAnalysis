f68a4cfe600e9cb0d2df7dbdc1a1b78eaac1fd51
n-kall <noa@tutamail.com>
2018-01-30 14:43:00 +0100

    ready to run

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript -e "install.packages(c('knitr', 'lme4', 'afex', 'cowplot', 'DHARMa', 'xtable', 'rockchalk', 'tikzDevice', 'parallel', 'fifer', 'multcomp', 'vcdExtra'), repos='https://cran.uni-muenster.de/')"
+Rscript -e "install.packages(c('knitr', 'memisc', 'lme4', 'afex', 'cowplot', 'DHARMa', 'xtable', 'rockchalk', 'tikzDevice', 'parallel', 'fifer', 'multcomp', 'vcdExtra'), repos='https://cran.uni-muenster.de/')"
