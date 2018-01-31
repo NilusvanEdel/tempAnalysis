4f9e11b574daed9e62e2bde7b4ec6ba7ed5414d7
n-kall <noa@tutamail.com>
2018-01-31 09:46:31 +0100

    add separate glmm files

---
+++ vr_sidewalk_glmm.R
@@ -0,0 +1,99 @@
+our_seed <- 123
+set.seed(our_seed)
+
+require(knitr)
+require(lme4)
+require(afex)
+library(memisc)
+require(cowplot)
+require(DHARMa)
+require(xtable)
+require(rockchalk)
+require(tikzDevice)
+require(parallel)
+require(fifer)
+require(multcomp)
+require(vcdExtra)
+require(catspec)
+
+# sidewalk
+# rename gender variable
+colnames(sidewalk.data)[2] <- "gender"
+# rename driver variable
+colnames(sidewalk.data)[3] <- "motorist"
+# set participant to factor
+sidewalk.data$participant.ID <- factor(sidewalk.data$participant.ID)
+# rename levels for driver
+sidewalk.data$motorist <- factor(sidewalk.data$motorist,
+                            levels = c("False", "True"),
+                            labels = c("human", "self-driving"))
+# rename levels for gender
+sidewalk.data$gender <- factor(sidewalk.data$gender,
+                            levels = c("False", "True"),
+                            labels = c("female", "male"))
+# set confidence to numeric
+sidewalk.data$confidence <- as.numeric(sidewalk.data$confidence)
+                                        # sanity check
+sidewalk.sub <- subset(sidewalk.data,
+                       sidewalk.data$passedSanCheck == "True" &
+                       SecondSanCheck == "True")
+
+sidewalk.sub$participant.ID <- droplevels(sidewalk.sub$participant.ID)
+
+
+sidewalk.sub$perspective <- factor(sidewalk.sub$perspective,
+                                      levels = c("Observer",
+                                                 "Passenger",
+                                                 "PedSmall",
+                                                 "PedLarge"),
+                                      labels = c("Bystander",
+                                                 "Passenger",
+                                                 "PedSmall",
+                                                 "PedLarge"))
+
+sidewalk.sub$age_c <- scale(sidewalk.sub$age)
+
+sidewalk.sub$perceivedIden <- factor(sidewalk.sub$perceivedIden,
+                                   levels = c("Beobachter",
+                                              "Mitfahrer",
+                                              "Fußgänger"),
+                                   labels = c("Bystander",
+                                              "Passenger",
+                                              "Pedestrian"))
+
+
+(nc <- detectCores())
+cl <- makeCluster(rep("localhost", nc))
+
+sidewalk_glmm <- mixed(decision ~ perspective + motorist +
+                        perspective:motorist +
+                        trial + gender + age_c + opinAV +
+                        education +  drivExperience + visImpairment +
+                        perceivedIden +
+                        (1 | participant.ID),
+                    method = "PB", # change to PB for final
+                    family = "binomial", data = sidewalk.sub,
+                    args_test = list(nsim = 1000, cl = cl), cl = cl,
+                    control = glmerControl(optimizer = "bobyqa",
+                                           optCtrl = list(maxfun = 2e5)))
+
+stopCluster(cl)
+
+
+sidewalk_glmm.resid <- simulateResiduals(
+    fittedModel = sidewalk_glmm$full_model, n = 2000)
+sidewalk_glmm_resid.plot <- plotSimulatedResiduals(
+    simulationOutput = sidewalk_glmm.resid)
+
+
+emm_sidewalk_i <- emmeans(sidewalk_glmm, pairwise ~ perspective | motorist,
+                          type = 'response')
+
+emm_sidewalk_persp <- emmeans(sidewalk_glmm, pairwise ~ perspective,
+                              type = 'response')
+
+
+emm_sidewalk_motorist <- emmeans(sidewalk_glmm, pairwise ~ motorist,
+                                 type = 'response')
+
+save.image(file = "vr_sidewalk_glmm.RData")
